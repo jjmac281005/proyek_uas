7221da63e172a8060b13bae65021180e09bc628f
GLitchyy123 <amandaevania123@gmail.com>
2025-05-01 13:11:37 +0700

    Upload PHP For Being Reserved

---
+++ get_reserved_seats.php
@@ -0,0 +1,13 @@
+<?php
+$koneksi = new mysqli("localhost", "root", "", "cafe_reservation");
+
+$result = $koneksi->query("SELECT seat_number FROM reservations");
+$seats = [];
+
+while ($row = $result->fetch_assoc()) {
+    $seats[] = $row;
+}
+
+header('Content-Type: application/json');
+echo json_encode($seats);
+?>
